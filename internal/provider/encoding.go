6df1390412b4a45ddc1d2f4fdbab061b167b42b8
谢祥伦 <xxl@realfun.cn>
2024-11-04 17:04:41 +0800

    refactor(provider): 移除未使用的 base32 导包
    - 删除了 encoding/base32 的导入语句
    - 移除了未使用的 b32Enc 变量

--- internal/provider/encoding.go
+++ internal/provider/encoding.go
@@ -4,9 +4,7 @@
 package provider
 
 import (
-	"encoding/base32"
 	"encoding/base64"
 )
 
-var b32Enc = base32.StdEncoding.WithPadding(base32.NoPadding)
 var b64Enc = base64.StdEncoding.WithPadding(base64.NoPadding)
